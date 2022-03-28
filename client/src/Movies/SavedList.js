98d17557b5b6f696429bd29b39e1518663a868b3
heartandkarma <heartandkarma@gmail.com>
2022-03-28 12:46:25 -0700

    made home button work

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function SavedList(props) {
   return (
-    <div className="saved-list">
+    <div className='saved-list'>
       <h3>Saved Movies:</h3>
-      {props.list.map(movie => (
-        <span className="saved-movie">{movie.title}</span>
+      {props.list.map((movie) => (
+        <span className='saved-movie'>{movie.title}</span>
       ))}
-      <div className="home-button">Home</div>
+      <div className='home-button'>
+        <Link to='/'>
+          <button>Home</button>
+        </Link>
+      </div>
     </div>
   );
 }
